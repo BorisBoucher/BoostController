52634dc14dffb330105025ac3f5afae6a4e8edc9
Boris Boucher <boris.boucher@gmail.com>
2020-05-16 19:17:51 +0200

    Added 12V booster management for stimulation board

--- BoostControler_Stimulator/src/main.cpp
+++ BoostControler_Stimulator/src/main.cpp
@@ -5,12 +5,17 @@
 // 
 
 #include <Arduino.h>
+#include <FastPID.h>
 
 #define OUT_RPM 13
 #define OUT_SPEED 12
 #define OUT_MAF 11
 #define IN_PLUS 7
 #define IN_MINUS 8
+#define OUT_VBOOST 3  // PWM out, control voltage booster
+#define IN_VBOOST_REF A0    // Analog in, target voltage booster : 0..5v => 5..15V
+#define IN_VBOOST A1    // Analog in, monitor voltage booster
+
 
 struct State
 {
@@ -24,7 +29,19 @@ struct State
   uint32_t mLastMafPulse = 0;
   uint32_t mMafHalfPeriod = 0;
   uint32_t mLastInputCheck = 0;
+
+  // Voltage booster control
+  uint16_t mInputV = 0;   // 1000th of volt
+  uint16_t mOutputV = 0;  // 1000th of volt
+  uint16_t mTargetV = 13 * 1000;  // 1000th of volt
+  uint16_t mLoopCount = 0;
 } gState;
+
+const double kI = 0.1 ;
+const double kP = 4.0;
+const double kD = 0.000;
+
+FastPID vBoostPid(kI, kP, kD, 1000);
 
 void setup() {
   pinMode(OUT_RPM, OUTPUT);
@@ -32,14 +49,52 @@ void setup() {
   pinMode(OUT_MAF, OUTPUT);
   pinMode(IN_PLUS, INPUT_PULLUP);
   pinMode(IN_MINUS, INPUT_PULLUP);
+  pinMode(OUT_VBOOST, OUTPUT);
 
   // Init serial for MUT simulation
   Serial.begin(1953);
+
+//  Serial.begin(9600);
+
+  // Init voltage booster PID
+//  vBoostPID.setMode(1);  // auto mode
+  vBoostPid.setOutputRange(0, 10 * 1000); // Clamp output to 10V => 5+10 ~15V
+
+  // Configure PWM output for 100KHz, with a maximum of 50% duty cycle
+  // Use Timer 2 and output 3
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS20); // no div
+//  TCCR2B = _BV(CS22); // /64
+  OCR2A = 0;
+  OCR2B = 0;
+}
+
+void updateVoltageBooster(uint32_t nowUs)
+{
+  static uint32_t lastUpdateUs = 0U;
+
+  if (nowUs - lastUpdateUs > 1000)
+  {
+    // Read input, value in 1000th of V. Read 1024 for 5V throw a voltage divider with 3.3k and 1k resistor. 
+//    gState.mTargetV = 5000 + round(analogRead(IN_VBOOST_REF) / 1.024 * 10.0);
+    gState.mInputV = round(analogRead(IN_VBOOST) * (1330.0f / 330.0f) / 1.024 * 5.0);
+  
+    gState.mOutputV = vBoostPid.step(gState.mTargetV, gState.mInputV);
+
+    // Compute PWM value
+//    OCR2B = (uint32_t(gState.mInputV) * 220) / 10000;
+    OCR2B = (uint32_t(gState.mOutputV) * 200) / 10000;
+
+    ++gState.mLoopCount;
+    lastUpdateUs = nowUs;
+  }
 }
 
 void loop() 
 {
   uint32_t now = micros();
+
+  updateVoltageBooster(now);
 
   if (now - gState.mLastInputCheck > 100000)
   {
@@ -120,6 +175,24 @@ void loop()
     digitalWrite(OUT_MAF, not digitalRead(OUT_MAF));
     gState.mLastMafPulse = now;
   }
+
+  // // Serial display
+  // static uint32_t lastDisplay = 0;
+  // if (now - lastDisplay > 1000000)
+  // {
+  //   Serial.print("VTGT = ");
+  //   Serial.println(gState.mTargetV / 1000.0);
+  //   Serial.print("VIN  = ");
+  //   Serial.println(gState.mInputV / 1000.0);
+  //   Serial.write("VOUT = ");
+  //   Serial.println(gState.mOutputV / 1000.0);
+  //   Serial.write("PWM = ");
+  //   Serial.println(OCR2B);
+  //   Serial.print("Loop/s = ");
+  //   Serial.println(gState.mLoopCount);
+  //   gState.mLoopCount = 0;
+  //   lastDisplay = now;
+  // }
 
   // MUT responder
   if (Serial.available() > 0)
